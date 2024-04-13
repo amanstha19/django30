5f383c5bbe76948df3514a0cf189c177108e049b
shresthaaman27@gmail.com <shresthaaman27@gmail.com>
2024-04-13 21:40:04 +0545

    context passing

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path 
-from .views import helloworld, home, portfolio, temp_inherit_home
+from .views import helloworld, home, portfolio, temp_inherit_home, temp_inherit_features, temp_inherit_pricing
 
 urlpatterns = [
 
     path("", home, name='myhome'),
     path('portfolio/', portfolio, name='portfolio_page'),
-    path('temp-inherit/', temp_inherit_home, name='temp_inherit_home')
+    path('temp-inherit/', temp_inherit_home, name='temp_inherit_home'),
+    path('features/', temp_inherit_features, name='temp_inherit_features'),
+    path('pricing/', temp_inherit_pricing, name="temp_inherit_pricing")
 ]
 
--- home/views.py
+++ home/views.py
@@ -14,4 +14,31 @@ def portfolio(request):
     return render(request, template_name='home/index.html')
 
 def temp_inherit_home(request):
-    return render(request, template_name='home/temp_inherit_home.html')
+
+
+      items =[{"name": "shirt", "store_location": "ktm", "price": "1000"},
+    {"name": "pants", "store_location": "bkt", "price": "200"},
+    {"name": "trousers", "store_location": "lalitpur", "price": "3000"},
+      ]
+
+
+
+      return render(request, template_name='home/temp_inherit_home.html', context={"name": "jon", "age": 30, "address": "KTM",
+                                                                                   "items": items})
+
+
+def temp_inherit_features(request):
+
+    name = [
+            {"name": "shirt ", "description": "100% pure cotton"},
+            {"name": "pants", "description": "very good fabric"},
+
+    ]
+
+
+    return render(request, template_name='home/temp_inherit_features.html', context = { "name": name })
+
+
+
+def temp_inherit_pricing(request):
+    return render(request, template_name='home/temp_inherit_pricing.html')

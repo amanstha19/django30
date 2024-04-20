0f1da1534ef56c79571a4a0ddd639d1661a7cc25
shresthaaman27@gmail.com <shresthaaman27@gmail.com>
2024-04-20 22:05:59 +0545

    more field added in add student profile

--- forms/views.py
+++ forms/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from home.models import ClassRoom, Student
+from home.models import ClassRoom, Student, StudentProfile
 
 
 def add_classroom(request):
@@ -21,7 +21,11 @@ def add_student(request):
         email = request.POST.get("email")
         address = request.POST.get("address")
         classroom_id = request.POST.get("classroom_id")
-        Student.objects.create(name=name, age=age, email=email, address=address, classroom_id=classroom_id)
+        student = Student.objects.create(name=name, age=age, email=email, address=address, classroom_id=classroom_id)
+        phone = request.POST.get("phone")
+        bio = request.POST.get("bio")
+        roll = request.POST.get("roll")
+        StudentProfile.objects.create(phone=phone, bio=bio, roll_no=roll, student=student)
         return redirect("student")
 
 

d1c39a2b79580acb1dd84fc24d0197b99bada492
shresthaaman27@gmail.com <shresthaaman27@gmail.com>
2024-04-24 17:09:21 +0545

    loop api

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from.views import hello_world
+from.views import hello_world,student_info
 
 urlpatterns = [
-     path("hello-world/", hello_world)
+     path("hello-world/", hello_world),
+     path("student-info/", student_info)
  ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 
 from django.http import JsonResponse
+from home.models import Student
 
 
 
@@ -8,3 +9,25 @@ def hello_world(request):
     return JsonResponse({
         "message": "Hello World "
     })
+
+
+def student_info(request):
+    students = Student.objects.all()
+    student_data = []
+    for student in students:
+        student_data.append({
+            "name": student.name,
+            "age": student.age,
+            "address": student.address
+        })
+    return JsonResponse(student_data, safe=False)
+
+
+
+# def student_info(request):
+#     students = Student.objects.get(id=1)
+#     return JsonResponse({"name": students.name,
+#             "age": students.age,
+#             "address": students.address
+#                          })
+#
